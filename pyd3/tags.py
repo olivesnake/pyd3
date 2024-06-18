2ab0ce1ad952c293021d1fdefec508d552d20595
oliver <oliverbcontact@gmail.com>
2024-06-18 16:00:29 -0400

    code rewrite, improved performance
    rewrote most functionality from beginning, get_tags now ~20% faster

--- pyd3/tags.py
+++ pyd3/tags.py
@@ -1,91 +1,104 @@
-# 2024 Oliver
-
-import string
+#
 import re
+import string
 from typing import List, Dict, Any
-from pydantic import BaseModel
-
-# http://www.multiweb.cz/twoinches/mp3inside.htm
-# https://mutagen-specs.readthedocs.io/en/latest/id3/id3v2.4.0-structure.html
 
 __all__ = ["get_tags", "Mp3Tag"]
-__version__ = "1.0.0"
 
-NULL = '\x00'
-TAG_OFFSET = 10  # bytes
-ID3v2_HEADER_LENGTH = 10  # bytes
 DEFAULT_ENCODING = 'ISO-8859-1'  # standard mp3 text encoding
 JPEG_SOI_MARKER = b'\xFF\xD8'  # start of image marker for jpeg files
 JPEG_EOI_MARKER = b'\xFF\xD9'  # end of image
+ID3v2_HEADER_LENGTH = 10
+IDENTIFIER_LEN = 3
+TAG_SIZE_START_IDX = 6
+TAG_OFFSET = 10
+INT_KEYS = ('track_number', 'disc_number', 'year')
+ARTIST_KEYS = ("composer", "artist1", "artist2")
+NULL = '\x00'
 
 FRAME_IDS = [b'TIT2', b'TALB', b'TPUB', b'TCON', b'TYER', b'TRCK',
              b'TOPS', b'TPE1', b'TPE2', b'TCOM', b'APIC', b'COMM', b'WXXX']
-
-METADATA_MAP = {
-    'TIT2': 'title', 'TALB': 'album', 'TPUB': 'publisher', 'TCON': 'genre',
-    'TYER': 'year', 'TRCK': 'track_number', 'TOPS': 'disc_number', 'TPE1': 'artist1',
-    'TPE2': 'artist2', 'TCOM': 'composer', 'APIC': 'artwork', 'COMM': 'comments', 'WXXX': 'url'
-}
-
 FRAME_ENCODING = {
     0: 'ISO-8859-1', 1: 'UTF-16', 2: 'UTF-16BE', 3: 'UTF-8'
 }
+METADATA_MAP = {
+    b'TIT2': 'title', b'TALB': 'album', b'TPUB': 'publisher', b'TCON': 'genre',
+    b'TYER': 'year', b'TRCK': 'track_number', b'TOPS': 'disc_number', b'TPE1': 'artist1',
+    b'TPE2': 'artist2', b'TCOM': 'composer', b'APIC': 'artwork', b'COMM': 'comments', b'WXXX': 'url'
+}
 
 
-class Mp3Tag(BaseModel):
+class Mp3Tag:
     album: str
-    artists: List[str]
+    artists: List[str]  # comprised of composer, artist1 and artist2 from mp3 tag
     artwork: bytes
     comments: str
     genre: str
     publisher: str
     title: str
     track_number: int
+    disc_number: int
     url: str
     year: int
 
 
-def extract_text(s) -> str:
-    a = re.sub(r'\x00', '', s)
-    return clean_string(a)
+def decode_synchsafe(x: int | bytes):
+    """
+    decode bytes/int from synchronization safe format to integer
+    https://phoxis.org/2010/05/08/synch-safe/
+    :param x: synchsafe value
+    :return: integer
+    """
+    if isinstance(x, bytes):
+        x = int.from_bytes(x, byteorder="big")
+
+    if (x & 0x808080):
+        # ignore MSB
+        x = x & 0x7f7f7f7f
+
+    ans = 0
+    a = x & 0xff
+    b = (x >> 8) & 0xff
+    c = (x >> 16) & 0xff
+    d = (x >> 24) & 0xff
+
+    ans = ans | a
+    ans = ans | (b << 7)
+    ans = ans | (c << 14)
+    ans = ans | (d << 21)
+
+    return ans
 
 
-def clean_string(s: str) -> str:
+def extract_text(data: bytes) -> str:
     """
-    remove nonprintable characters from a string
-    :param s: string
-    :return: string with only printable characters
+    builds a string out of printable characters from bytes
+    :param data: byte data
+    :return: string
     """
-    result = ''.join(filter(lambda c: c in string.printable, s))
-    return result
+    x = data.decode(DEFAULT_ENCODING)
+    res = []
+    for c in x:
+        if c in string.printable:
+            res.append(c)
+    return ''.join(res)
 
 
-def int_from_sync_safe(data: bytes) -> int:
+def extract_image_data(data: bytes, tag: bytes) -> bytes:
     """
-    gets integer value from synchronization safe data
-    :param data: binary sync safe data
-    :return: integer value
-    """
-    n = sum(b << ((3 - i) * 7) for i, b in enumerate(data))
-    return n
-
-
-def handle_image_frame(frame_data: bytes, tag: bytes) -> bytes:
-    """
-    extract image byte data from mp3 file frame
-    https://docs.fileformat.com/image/jpeg/
-    :param frame_data: frame data
-    :param tag: mp3 tag
-    :return: image bytes
-    """
+       extract image byte data from mp3 file frame
+       https://docs.fileformat.com/image/jpeg/
+       :param frame_data: frame data
+       :param tag: mp3 tag
+       :return: image bytes
+       """
     # get text encoding
-    text_encoding = frame_data[0]
+    text_encoding = data[0]
     encoding = FRAME_ENCODING[text_encoding]
     # find mime type and determine starting point of image data
-    match = re.search(r'image/[a-z]+', frame_data.decode(encoding))
-    mime_type = frame_data[match.start():match.end()]
+    match = re.search(r'image/[a-z]+', data.decode(encoding))
+    mime_type = data[match.start():match.end()]
     if mime_type != b'image/jpeg':
-        # TODO: add support for other MIME types
         return None
     # get first instance of jpeg image start
     img_start = tag.find(JPEG_SOI_MARKER)
@@ -95,92 +108,72 @@ def handle_image_frame(frame_data: bytes, tag: bytes) -> bytes:
     return img_data
 
 
-def handle_text_frame(frame_data, tag=None):
+def get_frame_data(tag: bytes, frame_id: bytes):
     """
-    extract desired string from text frames of the mp3 tag
-    :param frame_data: hex data frame
-    :param tag: mp3 tag (placeholder)
-    :return:
+    get frame tag content
+    :param tag: the tag bytes
+    :param frame_id: the frame identifier
+    :return: content
     """
-    text_content = clean_string(frame_data.decode(DEFAULT_ENCODING))
-    return text_content
+    start_idx = tag.find(frame_id)
+    if start_idx == -1:
+        return None
+    offset = start_idx + TAG_OFFSET
+    header = tag[start_idx:offset]
+    size = decode_synchsafe(header[4:8])
+    data = tag[offset:offset + size]
+    # extract desired content from bytes data
+    if frame_id == b'APIC':
+        content = extract_image_data(data, tag)
+    else:
+        content = extract_text(data)
 
-
-def _get_tag_data(tag, frame_id_bytes):
-    """"
-
-    :param tag:
-    :param frame_id_bytes:
-    :return:
-    """
-    tag_start_idx = tag.find(frame_id_bytes)
-    frame_header = tag[tag_start_idx:tag_start_idx + TAG_OFFSET]  # get the header for the tag
-    frame_id = frame_header[0:4].decode(DEFAULT_ENCODING)  # defines the tag (eg. Song title, artist)
-    frame_size = int_from_sync_safe(frame_header[4:8])  # how long the tag data is not including the header
-    frame_flags = frame_header[8:10]  # get any possible flags
-    offset = tag_start_idx + TAG_OFFSET
-    frame_data = tag[offset:offset + frame_size]
-    content = handle_image_frame(frame_data, tag) if frame_id == 'APIC' else handle_text_frame(frame_data, tag)
     return content
 
 
-def remove_keys(d, keys):
+def format_tags(frames: Dict[bytes, Any]) -> Mp3Tag:
     """
-    remove keys from a dictionary
-    :param d: dictionary
-    :param keys: list of keys to be removed
-    :return: dictionary with removed keys
+    tag frames collected in dictionary and
+    create Mp3Tag object for code completion and type checking
+    :param frames: dictionary of frame names and data
+    :return:
     """
-    for key in filter(lambda k: k in d, keys):
-        del d[key]
-    return d
-
-
-def prettify_tags(tag_frames) -> dict:
-    """
-    modify values so they are of desired formatting
-    :tag_frames:
-    :return: updated dictionary
-    """
-    tags = dict()
-    for k, v in tag_frames.items():
+    tag = Mp3Tag()
+    artists = set()
+    for k, v in frames.items():
         key = METADATA_MAP[k]
-        if key == 'track_number':
-            v = v.split('/')[0]
-        if key == 'track_number' or key == 'year':
-            v = int(v)
-        tags[key] = v
+        if key in ARTIST_KEYS and v is not None:
+            artists.add(v)
+            continue
+        if key in INT_KEYS and v is not None:
+            v = v.split('/')[0] if key == "track_number" else int(v)
 
-    # put all artists/composer into on key
-    v = set()
-    for k in ['artist1', 'artist2', 'composer']:
-        if tags.get(k):
-            v.add(tags[k])
+        tag.__setattr__(key, v)
 
-    artists = list(sorted(v))
-    tags['artists'] = artists
-    tags = remove_keys(tags, keys=['artist1', 'artist2', 'composer'])
-    tags = dict(sorted(tags.items()))
-    return tags
+    tag.artists = list(artists)
+    return tag
 
 
-def get_tags(filename: str) -> Mp3Tag:
+def get_tags(filename: str) -> Mp3Tag | None:
     """
     :param filename: mp3 filename
-    :return: dictionary of metadata from mp3 file
+    :return: Mp3Tags object of mp3 file tag attributes
     """
-    with open(filename, mode='rb') as mp3_file:
-        id3v2_header = mp3_file.read(ID3v2_HEADER_LENGTH)
-        if id3v2_header[0:3] != b'ID3':  # check if id3v2
-            return {}  # unsupported currently
-        # compute total metadata tag size and get tag data
-        tag_size = int_from_sync_safe(id3v2_header[6:10])
-        tag = mp3_file.read(tag_size)
+    # read data
+    with open(filename, "rb") as f:
+        # check if ID3v2 file (first 3 bytes should be ID3)
+        if f.read(3) != b'ID3':
+            return None
+        f.seek(6)  # skip to metadata tag size
+        # get the size of the tag
+        size = decode_synchsafe(f.read(4))
+        # get the mp3 tag
+        tag = f.read(size)
+
     frames = dict()
-    # get all (useful) tag data
-    for frame_id in filter(lambda fid: tag.find(fid) != -1, FRAME_IDS):
-        frames[frame_id.decode(DEFAULT_ENCODING)] = _get_tag_data(tag, frame_id)
-    # map tags to more developer/user friendly keys
-    tags = prettify_tags(frames)
-    tags = Mp3Tag(**tags)
-    return tags
+
+    for frame_id in FRAME_IDS:
+        # check for frame in tag
+        frames[frame_id] = get_frame_data(tag, frame_id)
+
+    return format_tags(frames)
